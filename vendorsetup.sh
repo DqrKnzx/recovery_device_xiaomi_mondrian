032684e5b157da33f5fb0ba64602fb20abac1cd0
DqrKnzx <DavidYonatan9@hotmail.com>
2023-05-25 17:22:05 +0000

    Bringup: OrangeFox Recovery

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_mondrian-user
-add_lunch_combo omni_mondrian-userdebug
-add_lunch_combo omni_mondrian-eng
+add_lunch_combo twrp_mondrian-user
+add_lunch_combo twrp_mondrian-userdebug
+add_lunch_combo twrp_mondrian-eng
